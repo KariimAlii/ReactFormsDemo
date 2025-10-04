8f0fd5488b50bf11cf18aa7a797aede01bf3249b
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 18:39:02 +0300

    Validate Form Inputs on every keystroke using State

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,6 +6,8 @@ export default function Login() {
         password: ''
     });
 
+    const isEmailInvalid = !enteredValues.email.includes('@gmail');
+
     function handleSubmit(event) {
         event.preventDefault();
         console.log("SUBMITTED")
@@ -40,6 +42,9 @@ export default function Login() {
                         onChange={(event) => handleInputChange('email', event.target.value)}
                         value={enteredValues.email}
                     />
+                    <div className="control-error">
+                        {isEmailInvalid && <p>Please enter a valid email address</p>}
+                    </div>
                 </div>
 
                 <div className="control no-margin">
