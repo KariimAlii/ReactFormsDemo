f877a4795656c974951ee9da53814b1f88af583a
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 17:59:23 +0300

    Prevent the button from submitting the form by changing its type

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 ﻿export default function Login() {
     function HandleSubmit() {
-
+        console.log("SUBMITTED")
     }
 
     return (
@@ -22,7 +22,7 @@
 
             <p className="form-actions">
                 <button className="button button-flat">Reset</button>
-                <button className="button" onClick={HandleSubmit}>Login</button>
+                <button type="button" className="button" onClick={HandleSubmit}>Login</button>
             </p>
         </form>
     );
