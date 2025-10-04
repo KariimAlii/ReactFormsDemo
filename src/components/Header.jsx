373c43880f92d57cec15b4f6e2345ada080a1d25
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 17:49:14 +0300

    Added Starter Template

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+﻿import logoImg from '../assets/logo.jpg';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={logoImg} alt="A form and a pencil" />
+            <h1>React Forms</h1>
+        </header>
+    );
+}
