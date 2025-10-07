65da3faba30f4dac1daa1c1b39450c836e17aa4d
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-08 01:02:17 +0300

    Building Custom Reusable Input Component (2)

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,10 @@
-﻿export default function Input({ label, id, type, name, onBlur, onChange, value, error, ...props }) {
+﻿export default function Input({ label, id, error, ...props }) {
     return (
         <div className="control no-margin">
             <label htmlFor={id}>{ label }</label>
             <input
                 id={id}
-                type={type}
-                name={name}
                 {...props}
-                onBlur={onBlur}
-                onChange={onChange}
-                value={value}
             />
             <div className="control-error">
                 {error && <p>{error}</p>}
